60f930f596619554734472689d79c63452d21668
Sara Kurdi <sarakurdi@Saras-MacBook-Pro.local>
2020-08-27 19:30:08 -0400

    Saves hits in a TTree.

--- cmhitsPHG4.cpp
+++ cmhitsPHG4.cpp
@@ -489,11 +489,11 @@ int cmhitsPHG4() {
   tree->Branch("pad",hit);
 
   for (int i=0;i<tree->GetEntries();i++){
-    hit=(Hits[i]);
+    hit=(&Hits[i]);
     tree->Fill();
   }
   
-  tree->SaveAs("phg4hitsTree.pdf");
+  tree->SaveAs("phg4hitsTree");
 
     
   return 0;
