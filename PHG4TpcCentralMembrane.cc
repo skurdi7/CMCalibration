212a710c7a972c92d78228994c3e73765b838991
Sara Kurdi <sarakurdi@Saras-MacBook-Pro.local>
2020-08-31 21:10:16 -0400

    Pushing files needed to compile makefile.

--- PHG4TpcCentralMembrane.cc
+++ PHG4TpcCentralMembrane.cc
@@ -1,3 +1,5 @@
+#include "PHG4TpcCentralMebrane.h"
+
 #include <iostream>
 #include <cmath>
 #include <vector>
@@ -9,100 +11,11 @@
 #include <g4main/PHG4Hitv1.h>
 R__LOAD_LIBRARY(libphg4hit.so)
 
-
 // all distances in mm, all angles in rad
 // class that generates stripes and dummy hit coordinates
 // stripes have width of one mm, length of one pad width, and are centered in middle of sector gaps
 
 using namespace std;
-
-class StripesClass {
-public:
-  StripesClass(); //default constructor
-  int getSearchResult(double xcheck, double ycheck); // check if coords are in a stripe
-
-  double begin_CM, end_CM; // inner and outer radii of central membrane
-  
-  vector<PHG4Hitv1*> PHG4Hits;
-  
-private:
-  static const int nRadii = 8;
-  static const int nStripes_R1 = 6;
-  static const int nStripes_R2 = 8;
-  static const int nStripes_R3 = 12;
-
-  const double mm = 1.0;
-  const double cm = 10.0;
-  
-  int nPads_R1;
-  int nPads_R2;
-  int nPads_R3;
-  
-  double padfrac_R1;
-  double padfrac_R2;
-  double padfrac_R3;
-  double str_width; // width of a stripe
-  double arc_r; // radius of arc on end of a stripe
-  double R1_e[nRadii], R1[nRadii], R2[nRadii], R3[nRadii];
-  
-  double spacing_R1_e[nRadii], spacing_R1[nRadii], spacing_R2[nRadii], spacing_R3[nRadii];
-  
-  //bottom left - 1a
-  double x1a_R1_e[nStripes_R1][nRadii], y1a_R1_e[nStripes_R1][nRadii];
-  double x1a_R1[nStripes_R1][nRadii], y1a_R1[nStripes_R1][nRadii];
-  double x1a_R2[nStripes_R2][nRadii], y1a_R2[nStripes_R2][nRadii];
-  double x1a_R3[nStripes_R3][nRadii], y1a_R3[nStripes_R3][nRadii];
-  
-  //bottom right - 1b
-  double x1b_R1_e[nStripes_R1][nRadii], y1b_R1_e[nStripes_R1][nRadii];
-  double x1b_R1[nStripes_R1][nRadii], y1b_R1[nStripes_R1][nRadii];
-  double x1b_R2[nStripes_R2][nRadii], y1b_R2[nStripes_R2][nRadii];
-  double x1b_R3[nStripes_R3][nRadii], y1b_R3[nStripes_R3][nRadii];
-  
-  //top left - 2a
-  double x2a_R1_e[nStripes_R1][nRadii], y2a_R1_e[nStripes_R1][nRadii];
-  double x2a_R1[nStripes_R1][nRadii], y2a_R1[nStripes_R1][nRadii];
-  double x2a_R2[nStripes_R2][nRadii], y2a_R2[nStripes_R2][nRadii];
-  double x2a_R3[nStripes_R3][nRadii], y2a_R3[nStripes_R3][nRadii];
-  
-  //top right - 2b
-  double x2b_R1_e[nStripes_R1][nRadii], y2b_R1_e[nStripes_R1][nRadii];
-  double x2b_R1[nStripes_R1][nRadii], y2b_R1[nStripes_R1][nRadii];
-  double x2b_R2[nStripes_R2][nRadii], y2b_R2[nStripes_R2][nRadii];
-  double x2b_R3[nStripes_R3][nRadii], y2b_R3[nStripes_R3][nRadii];
-  
-  //left midpoint - 3a
-  double x3a_R1_e[nStripes_R1][nRadii], y3a_R1_e[nStripes_R1][nRadii];
-  double x3a_R1[nStripes_R1][nRadii], y3a_R1[nStripes_R1][nRadii];
-  double x3a_R2[nStripes_R2][nRadii], y3a_R2[nStripes_R2][nRadii];
-  double x3a_R3[nStripes_R3][nRadii], y3a_R3[nStripes_R3][nRadii];
-  
-  //right midpoint - 3b
-  double x3b_R1_e[nStripes_R1][nRadii], y3b_R1_e[nStripes_R1][nRadii];
-  double x3b_R1[nStripes_R1][nRadii], y3b_R1[nStripes_R1][nRadii];
-  double x3b_R2[nStripes_R2][nRadii], y3b_R2[nStripes_R2][nRadii];
-  double x3b_R3[nStripes_R3][nRadii], y3b_R3[nStripes_R3][nRadii];
-  
-  //Check which stripes get removed
-  int nGoodStripes_R1_e[nRadii];
-  int nGoodStripes_R1[nRadii];
-  int nGoodStripes_R2[nRadii];
-  int nGoodStripes_R3[nRadii];
-  int keepThisAndAfter[nRadii]; //min stripe index
-  int keepUntil_R1_e[nRadii]; //max stripe index
-  int keepUntil_R1[nRadii];
-  int keepUntil_R2[nRadii];
-  int keepUntil_R3[nRadii];
-  int result;
-
-  int nElectrons;
-  
-  void CalculateVertices(int nStripes, int nPads, double R[], double spacing[], double x1a[][nRadii], double y1a[][nRadii], double x1b[][nRadii], double y1b[][nRadii], double x2a[][nRadii], double y2a[][nRadii], double x2b[][nRadii], double y2b[][nRadii], double x3a[][nRadii], double y3a[][nRadii], double x3b[][nRadii], double y3b[][nRadii], double padfrac, int nGoodStripes[], int keepUntil[]);
-  
-  int SearchModule(int nStripes, double x1a[][nRadii], double x1b[][nRadii], double x2a[][nRadii], double x2b[][nRadii], double y1a[][nRadii], double y1b[][nRadii], double y2a[][nRadii], double y2b[][nRadii], double x3a[][nRadii], double y3a[][nRadii], double x3b[][nRadii], double y3b[][nRadii], double x, double y, int nGoodStripes[]);
-  
-  PHG4Hitv1* GetPHG4HitFromStripe(int petalID, int moduleID, int radiusID, int stripeID, int nElectrons);
-};
 
 StripesClass::StripesClass()
   : R1_e {227.0902789 * mm, 238.4100043 * mm, 249.7297296 * mm, 261.049455 * mm, 272.3691804 * mm, 283.6889058 * mm, 295.0086312 * mm, 306.3283566 * mm},
---
+++ PHG4TpcCentralMembraneLinkDef.h
@@ -0,0 +1,6 @@
+
+#ifdef __CINT__
+
+#pragma link C++ class PHG4TpcCentralMembrane - !;
+
+#endif /* __CINT__ */
