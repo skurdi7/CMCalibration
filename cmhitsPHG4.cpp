048054a25442fa93a908d6af4b30d0d133fd0405
Sara Kurdi <sarakurdi@Saras-MacBook-Pro.local>
2020-08-31 22:57:31 -0400

    Includes class rather than having it in the code.

--- cmhitsPHG4.cpp
+++ cmhitsPHG4.cpp
@@ -5,7 +5,7 @@
 #include "TVector3.h"
 #include "PHG4TpcCentralMembrane.h"
 #include "TTree.h"
-R__LOAD_LIBRARY(.libs/libg4tpccentralmembrane)
+R__LOAD_LIBRARY(.libs/libg4tpccentralmembrane.so)
 
 //from phg4tpcsteppingaction.cc
 #include <g4main/PHG4Hit.h>
