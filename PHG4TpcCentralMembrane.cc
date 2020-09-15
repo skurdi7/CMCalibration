a1b59eecbbf559454242621d0be34ebc36e7bf8b
Sara Kurdi <sarakurdi@Saras-MacBook-Pro.local>
2020-09-14 21:40:52 -0400

    Identifying stripe IDs and plotting them.

--- PHG4TpcCentralMembrane.cc
+++ PHG4TpcCentralMembrane.cc
@@ -399,8 +399,12 @@ int StripesClass::getStripeID(double xcheck, double ycheck){
 	  // calculate slope n then do dist
 	  
 	  m = (y3b_R1_e[i][j] - y3a_R1_e[i][j])/(x3b_R1_e[i][j] - x3a_R1_e[i][j]);
-	  cout << "x: " << xcheck << endl;
-	  cout << "y: " << ycheck << endl;
+	  cout << "y2: " << y3b_R1_e[i][j] << endl;
+	  cout << "y1: " << y3a_R1_e[i][j] << endl;
+	  cout << "x2: " << x3b_R1_e[i][j] << endl;
+	  cout << "x1: " << x3a_R1_e[i][j] << endl;
+	  cout << "xc: " << xcheck << endl;
+	  cout << "yc: " << ycheck << endl;
 	  cout << "m: " << m << endl;
 	  //cout << fabs((-m)*xcheck + ycheck) << endl;
 	  dist = fabs((-m)*xcheck + ycheck)/sqrt(1 + m*m);
