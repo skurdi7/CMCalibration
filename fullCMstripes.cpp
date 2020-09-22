87a2ce96f60146935bdea7c2da98ffa04b1b1e93
Sara Kurdi <sarakurdi@Saras-MacBook-Pro.local>
2020-09-22 14:09:14 -0400

    Fixed nStripesBefore.

--- fullCMstripes.cpp
+++ fullCMstripes.cpp
@@ -12,7 +12,9 @@ R__LOAD_LIBRARY(build/.libs/libg4tpccentralmembrane)
 
 int fullCMstripes() {
   StripesClass stripes;
-
+  ofstream eagle;
+  eagle.open ("eaglestripes.txt");
+  
   vector<PHG4Hitv1*> BV = stripes.BotVertices;
   vector<PHG4Hitv1*> TV = stripes.TopVertices;
   double xbl, xbr, xtl, xtr;
@@ -30,15 +32,14 @@ int fullCMstripes() {
     ytl = TV[i]->get_y(0);
     ytr = TV[i]->get_y(1);
 
-    ofstream eagle;
-    eagle.open ("eaglestripes.txt");
+  
     eagle << "wire x1=\"" << xbl << "\" y1=\"" << ybl << "\" x2=\""<< xtl << "\" y2=\"" << ytl << "\" width=\"0.1524\" layer=\"46\" curve=\"-180\"/> " << endl;
     eagle << "<wire x1=\"" << xtl << "\" y1=\"" << ytl << "\" x2=\""<< xtr << "\" y2=\"" << ytr << "\" width=\"0.1524\" layer=\"46\"/> " << endl;
     eagle << "<wire x1=\"" << xtr << "\" y1=\"" << ytr << "\" x2=\""<< xbr << "\" y2=\"" << ybr << "\" width=\"0.1524\" layer=\"46\" curve=\"-180\"/> " << endl;  
     eagle << "<wire x1=\"" << xbr << "\" y1=\"" << ybr << "\" x2=\""<< xbl << "\" y2=\"" << ybl << "\" width=\"0.1524\" layer=\"46\"/> " << endl;
     eagle << endl;
-    eagle.close();
+ 
   }
-  
+  eagle.close();
   return 0;
 }
