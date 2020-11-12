23100200b1bde83f6014812e778626c9cab0ecf7
Sara Kurdi <sarakurdi@Saras-MacBook-Pro.local>
2020-11-12 12:36:26 -0500

    Debugging shift interpolation.

--- cmShiftPlots.cpp
+++ cmShiftPlots.cpp
@@ -236,6 +236,9 @@ int cmShiftPlots() {
       for(int k = 0; k < nz; k++){
 	double z = minz + ((maxz - minz)/(1.0*nz))*(k+0.5); //center of bin
 
+	cout << "phi: " << phi;
+	cout << "r: " << r;
+	cout << "z: " << z << endl;
 	//double shiftreco =  hCMModel->Interpolate(phi,r,z);
 	double shifttrue = shifter.hR->Interpolate(phi,r,z);
 	//double difference = shiftreco - shifttrue; // try interpolation separately and check
