fafdf30099e9554b3ec9b7f04d474b43a72c90aa
Sara Kurdi <sarakurdi@Saras-MacBook-Pro.local>
2021-04-24 17:38:06 -0400

    Plotting total distortions.

--- cmShiftPlots.cpp
+++ cmShiftPlots.cpp
@@ -65,12 +65,12 @@ Shifter::Shifter(TString sourcefilename){
   hPhi->Add(hPhiave,-1);*/
 
 
-  hX=(TH3F*)average->Get("hIntDistortionX");
-  hY=(TH3F*)average->Get("hIntDistortionY");
-  hZ=(TH3F*)average->Get("hIntDistortionZ");
+  hX=(TH3F*)average->Get("hIntDistortionPosX");
+  hY=(TH3F*)average->Get("hIntDistortionPosY");
+  hZ=(TH3F*)average->Get("hIntDistortionPosZ");
   //set aves to 0 to see just fluct
-  hR=(TH3F*)average->Get("hIntDistortionR");
-  hPhi=(TH3F*)average->Get("hIntDistortionP");
+  hR=(TH3F*)average->Get("hIntDistortionPosR");
+  hPhi=(TH3F*)average->Get("hIntDistortionPosP");
   
   back=TFile::Open("/sphenix/user/rcorliss/distortion_maps/averages/empty.2sides.3d.file0.h_Charge_0.real_B1.4_E-400.0.ross_phi1_sphenix_phislice_lookup_r26xp40xz40.distortion_map.hist.root","READ"); //tells it only to read, not to write anything you make there.
    
