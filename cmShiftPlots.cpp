bb8a0ba7aba0893cbd2908788feb028bcded03e3
Sara Kurdi <sarakurdi@Saras-MacBook-Pro.local>
2021-04-24 18:04:38 -0400

    Plotting average distortion.

--- cmShiftPlots.cpp
+++ cmShiftPlots.cpp
@@ -51,12 +51,12 @@ Shifter::Shifter(TString sourcefilename){
   //average=TFile::Open("/gpfs/mnt/gpfs02/sphenix/user/rcorliss/distortion_maps/averages/empty.2sides.3d.file0.h_Charge_0.real_B1.4_E-400.0.ross_phi1_sphenix_phislice_lookup_r26xp40xz40.distortion_map.hist.root","READ"); 
   average=TFile::Open("/sphenix/user/rcorliss/distortion_maps/2021.04/apr07.average.real_B1.4_E-400.0.ross_phi1_sphenix_phislice_lookup_r26xp40xz40.distortion_map.hist.root","READ"); //updated average
   
-  hXave=(TH3F*)average->Get("hIntDistortionX");
+  /*hXave=(TH3F*)average->Get("hIntDistortionX");
   hYave=(TH3F*)average->Get("hIntDistortionY");
   hZave=(TH3F*)average->Get("hIntDistortionZ");
   //set aves to 0 to see just fluct
   hRave=(TH3F*)average->Get("hIntDistortionR");
-  hPhiave=(TH3F*)average->Get("hIntDistortionP");
+  hPhiave=(TH3F*)average->Get("hIntDistortionP");*/
 
   /*hX->Add(hXave,-1);
   hY->Add(hYave,-1);
