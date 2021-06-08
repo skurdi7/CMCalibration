9dcaffcafdacb79aa40959ebc295b151970f8c50
Sara Kurdi <sarakurdi@Saras-MacBook-Pro.local>
2021-06-07 23:36:11 -0400

    Printing histogram to pdf

--- CMDistortionReco.C
+++ CMDistortionReco.C
@@ -79,7 +79,7 @@ int CMDistortionReco() {
   newposition = new TVector3(1.,1.,1.);
 
   //histogram to compare times
-    TH1F *hTimePerEvent = new TH1F("hTimePerEvent","Time Per Event; time (ms)",20,0,10000);
+    TH1F *hTimePerEvent = new TH1F("hTimePerEvent","Time Per Event; time (ms)",10,0,2000);
     
   for (int ifile=0;ifile < nEvents;ifile++){
     //for each file, find all histograms in that file
